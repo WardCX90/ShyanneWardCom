8632044fb1bba2e432b76fb26e3cdcd2ae57c597
Cody Ward <wardx904@gmail.com>
2016-05-05 22:54:55 -0400

    fixed menu dropdown toggle issue

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 // HELPER FUNCTION THAT CLOSES MOBILE NAV AFTER CLICKING ANCHOR LINK
 
-$(function(){ 
-     var navMain = $("#nav-main");
-     navMain.on("click", "a", null, function () {
-         navMain.collapse('hide');
-     });
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
 });
